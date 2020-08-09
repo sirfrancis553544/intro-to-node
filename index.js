6520a55d75276612f6885fa24d7a577c642f296f
SirFrancis <55309898+sirfrancis553544@users.noreply.github.com>
2020-08-09 19:02:29 +0800

    8/9/2020

---
+++ index.js
@@ -0,0 +1,8 @@
+var superheroes = require("superheroes");
+const supervillains = require("supervillains");
+
+var mySuperheroName = superheroes.random();
+console.log("My superhero name is " + mySuperheroName);
+
+const mySupervillainName = supervillains.random();
+console.log("My supervillain name is " + mySupervillainName);
